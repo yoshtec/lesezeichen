881bcd9f028537470a7d6420e273c79f53a1ac28
yoshtec <yoshtec@gmx.net>
2021-11-10 08:27:55 +0100

    added completion and open url

--- lesezeichen/cli.py
+++ lesezeichen/cli.py
@@ -3,18 +3,43 @@
 # SPDX-License-Identifier: MIT
 
 import click
+from click.shell_completion import CompletionItem
+from lesezeichen.lesezeichen import Lesezeichen
+
+
+class UrlName(click.ParamType):
+    def shell_complete(self, ctx, param, incomplete):
+        lzn = Lesezeichen()
+        return [
+            CompletionItem(name, help=url)
+            for name, url in lzn.search_by_id(search=incomplete).items()
+        ]
+
 
 @click.group()
+@click.version_option()
 def cli():
     """ Organize and start bookmarks """
 
+@cli.command()
+@click.argument("name", type=UrlName())
+def open(name: str):
+    """Open url with the name"""
+    lzn = Lesezeichen()
+    lzn.open(name)
 
-@cli.command
-@click.argument("id")
+
+@cli.command()
+@click.argument("name")
 @click.argument("url")
 def add(
-        id: str,
+        name: str,
         url: str
 ):
-    pass
+    """add """
+    lzn = Lesezeichen()
+    lzn.add(name, url)
+    lzn.save()
+
+
 
--- lesezeichen/lesezeichen.py
+++ lesezeichen/lesezeichen.py
@@ -5,28 +5,31 @@
 import fnmatch
 import json
 import pathlib
+import subprocess
 
 
 class Lesezeichen:
 
     def __init__(self):
-        self.loaded = False
         self.bookmarks = dict()
+        self.load()
 
     @staticmethod
     def _file():
         return pathlib.Path.home() / ".lzn_lesezeichen"
 
     def load(self):
-        self.bookmarks = json.load(self._file().open("r"))
+        file = self._file()
+        if file.exists():
+            self.bookmarks = json.load(file.open("r"))
 
     def save(self):
         if self.bookmarks is not None and any(self.bookmarks):
             json.dump(self.bookmarks, self._file().open("w"))
 
-    def get(self, title: str):
-        if title in self.bookmarks:
-            return title
+    def get_url(self, name: str):
+        if name in self.bookmarks:
+            return self.bookmarks[name]
 
     def add(self, title: str, url: str):
         self.bookmarks[title] = url
@@ -36,3 +39,10 @@ class Lesezeichen:
 
     def search(self, search: str):
         return {k: v for k, v in self.bookmarks.items() if search in v or search in k}
+
+    def open(self, name: str):
+        url = self.get_url(name)
+        if url is not None:
+            subprocess.check_call(["open", url])
+        else:
+            print(f"no url with name={name}")
